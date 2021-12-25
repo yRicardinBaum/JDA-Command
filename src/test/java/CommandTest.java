19e3142abbf56549ca556d16f4c5da804f4cd67d
Ricardo <ricardinbaum@gmail.com>
2021-12-25 18:49:17 -0300

    teste

--- src/test/java/CommandTest.java
+++ src/test/java/CommandTest.java
@@ -18,3 +18,4 @@ public class CommandTest extends CommandExecutor {
         });
     }
 }
+
--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -1,10 +1,5 @@
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.requests.GatewayIntent;
-import net.dv8tion.jda.api.utils.ChunkingFilter;
-import net.dv8tion.jda.api.utils.Compression;
-import net.dv8tion.jda.api.utils.MemberCachePolicy;
-import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
 import javax.security.auth.login.LoginException;
 
@@ -12,19 +7,11 @@ public class TestMain {
     public static JDA bot;
     public static RiCommand settings;
 
-    public static void main(String[] args) throws LoginException, InterruptedException {
-        JDABuilder builder = JDABuilder.createDefault("ODk0MzI1MjU2MDU4MTk2MDQw.YVoXTA.KaZr6fZV9an7sddnrS5TGHPnZk4")
-                .setBulkDeleteSplittingEnabled(false)
-                .disableCache(CacheFlag.ACTIVITY)
-                .setMemberCachePolicy(MemberCachePolicy.VOICE.or(MemberCachePolicy.OWNER))
-                .setChunkingFilter(ChunkingFilter.NONE)
-                .disableIntents(GatewayIntent.GUILD_PRESENCES, GatewayIntent.GUILD_MESSAGE_TYPING)
-                .setCompression(Compression.NONE)
-                .setAutoReconnect(true)
-                .setLargeThreshold(50);
+    public static void main(String[] args) throws LoginException {
+        JDABuilder builder = JDABuilder.createDefault("ODk0MzI1MjU2MDU4MTk2MDQw.YVoXTA.FVZx8zfUaIGZui8zvFIf59pV8nU");
         bot = builder.build();
-        bot.awaitReady();
-        RiCommand cmds = new RiCommand(bot, "ri!").addCommand(new CommandTest());
+        RiCommand cmds = new RiCommand(bot, "ri!");
+        cmds.addCommand(new CommandTest());
         cmds.startCommands();
         settings = cmds;
     }
